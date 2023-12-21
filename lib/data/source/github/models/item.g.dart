4aff6a51b748da37f6a6b424abc231fee1a957a4
Atik-hridoy <atikahmedridoy@gmail.com>
2023-12-22 02:13:12 +0600

    first commit

---
+++ lib/data/source/github/models/item.g.dart
@@ -0,0 +1,19 @@
+part of 'item.dart';
+
+GithubItemModel _$GithubItemModelFromJson(Map<String, dynamic> json) {
+  $checkKeys(json, disallowNullValues: const ['name', 'imageurl', 'effect']);
+  return GithubItemModel(
+    json['name'] as String,
+    json['category'] as String? ?? '',
+    json['imageurl'] as String,
+    json['effect'] as String,
+  );
+}
+
+Map<String, dynamic> _$GithubItemModelToJson(GithubItemModel instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'category': instance.category,
+      'imageurl': instance.imageurl,
+      'effect': instance.effect,
+    };
